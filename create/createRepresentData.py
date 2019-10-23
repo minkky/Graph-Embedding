44b6d438bda1b87903ad32a1679d6ae18d04443e
minkky <minjiseo95@gmail.com>
2019-10-23 16:10:36 +0900

    create group data

--- create/createRepresentData.py
+++ create/createRepresentData.py
@@ -2,13 +2,13 @@
 test = [[0 for i in range(13)] for i in range(13)]
 maxi = -1
 while True:
-	inp = input()
+	inp = input('u v: ')
 	if inp == 'end':
 		break
-	a, b = list(map(int, inp.split(' ')))
-	test[a][b] = test[b][a] = 1
+	a, b= list(map(int, inp.split(' ')))
+	test[a][b] = test[b][a] = float(input('weight: '))
 	maxi = max(maxi, max(a, b))
 
-with open('represent/original7.txt', 'w') as file:
+with open('represent/original2.txt', 'w') as file:
 	for t in test[:maxi+1]:
 		file.write(' '.join(str(i) for i in t[:maxi+1]) + "\n")
